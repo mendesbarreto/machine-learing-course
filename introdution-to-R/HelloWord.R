a0bb34ac1ef664143035c513c4f16cea67d51a39
Douglas Mendes <mendes-barreto@live.com>
2019-04-10 16:28:55 -0400

    Add new gitignore

---
+++ introdution-to-R/HelloWord.R
@@ -0,0 +1,12 @@
+# Title     : TODO
+# Objective : TODO
+# Created by: mendesbarreto
+# Created on: 2019-04-07
+
+install.packages("lmtest", repos = "http://cran.us.r-project.org")
+install.packages("tidyr", repos = "http://cran.us.r-project.org")
+install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+library(ggplot2)
+
+a <- "Douglas"
+print(a)
