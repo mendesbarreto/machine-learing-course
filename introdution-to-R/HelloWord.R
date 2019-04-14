69277171167cd956517ef58cbaa8c9536ff63e58
Douglas Mendes <mendes-barreto@live.com>
2019-04-14 09:28:23 -0400

    Print variable

--- introdution-to-R/HelloWord.R
+++ introdution-to-R/HelloWord.R
@@ -1,15 +1,15 @@
-# Title     : TODO
-# Objective : TODO
+# Title     : Fist time on R
+# Objective : Create a script to read a json
 # Created by: mendesbarreto
 # Created on: 2019-04-07
 
 
-#%% md
-install.packages("lmtest", repos = "http://cran.us.r-project.org")
-install.packages("tidyr", repos = "http://cran.us.r-project.org")
-install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+#install.packages("lmtest", repos = "http://cran.us.r-project.org")
+#install.packages("tidyr", repos = "http://cran.us.r-project.org")
+#install.packages("ggplot2", repos = "http://cran.us.r-project.org")
+
 library(ggplot2)
 
 a <- "Douglas"
 print(a)
-#%% md
+
