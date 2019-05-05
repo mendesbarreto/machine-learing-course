9d2f598a5cc241baff4dc9e8fce79de04f4aefa6
Douglas Mendes <mendes-barreto@live.com>
2019-05-05 13:39:38 -0400

    Add filter to dataset to get only the relevant correlated data

--- introdution-to-python/dataset_cleaner.py
+++ introdution-to-python/dataset_cleaner.py
@@ -5,16 +5,23 @@ import matplotlib.pyplot as pyplot
 from pandas.core.generic import NDFrame
 
 
-class DataSetCleaner:
-    def remove_empty_values(self, data_frame: NDFrame) -> NDFrame:
-        modified_data_set = data_frame.fillna(" ")
-        sum_empty_values = panda.isnull(modified_data_set).sum()
+def remove_last_column(data_frame: NDFrame) -> NDFrame:
+    return data_frame.iloc[:-1, :-1]
 
-        if sum_empty_values.any():
-            print("Has some empties values in the data frame")
-            raise Exception("Problem in load data set, we need to remove them")
 
-        return modified_data_set
+def filter_value_from(data_frame: NDFrame, threshold: float, correlation_value_excluded=1.0):
+    filtered_data_frame: NDFrame = data_frame[abs(data_frame) > threshold][data_frame != correlation_value_excluded]
+    filtered_unstack = filtered_data_frame.unstack()
+    filtered_dropna = filtered_unstack.dropna()
+    return filtered_dropna.to_dict()
 
-    def remove_last_column(self, data_frame: NDFrame) -> NDFrame:
-        return data_frame.iloc[:-1, :-1]
+
+def remove_empty_values(data_frame: NDFrame) -> NDFrame:
+    modified_data_set = data_frame.fillna(" ")
+    sum_empty_values = panda.isnull(modified_data_set).sum()
+
+    if sum_empty_values.any():
+        print("Has some empties values in the data frame")
+        raise Exception("Problem in load data set, we need to remove them")
+
+    return modified_data_set
--- introdution-to-python/main.py
+++ introdution-to-python/main.py
@@ -3,7 +3,7 @@ import numpy.random as random
 import numpy
 from pandas.core.generic import NDFrame
 
-from dataset_cleaner import DataSetCleaner
+from dataset_cleaner import *
 import matplotlib.pyplot as pyplot
 
 from sklearn.datasets import load_boston
@@ -18,16 +18,23 @@ data_frame: NDFrame = panda.DataFrame(dataset.data, columns=dataset.feature_name
 data_frame['target'] = dataset.target
 
 # Clean the data
-data_frame_cleaner = DataSetCleaner()
-data_frame = data_frame_cleaner.remove_empty_values(data_frame)
+data_frame = remove_empty_values(data_frame)
 
 data_frame_correlation = data_frame.corr(method='pearson')
-data_frame_correlation = data_frame_cleaner.remove_last_column(data_frame_correlation)
+data_frame_correlation = remove_last_column(data_frame_correlation)
 
+description = load_boston()['DESCR']
+print(description)
 
+# This value describe the minimum value of approximation
+threshold = .5
+correlation_number_not_allowed = 1.0
 
+data_frame_important_correlation = filter_value_from(data_frame_correlation,
+                                                     threshold,
+                                                     correlation_number_not_allowed)
 
-print(dataset.target)
+print(data_frame_important_correlation)
 
 
 
